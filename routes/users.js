96c1a5f9caa5145468dfed20a9e0a17831fd2161
Nasacodes <Nasacodes@gmail.com>
2021-03-10 07:24:08 +0100

    init

--- routes/users.js
+++ routes/users.js
@@ -2,20 +2,20 @@ const express = require("express");
 const router = express.Router();
 const db = require("../models");
 const moment = require('moment');
-const {validateDate} = require("../validateDate");
+const {isValidDate} = require("../validateDate");
 
 // Register
 router.post("/register", (req, res) => {
-  let dateOfbirth = validateDate(req.body.dateOfbirth);
-  if (dateOfbirth.valid) {
+  let dateOfbirth = isValidDate(req.body.dateOfbirth);
+  if (dateOfbirth.isValid) {
     db.user
-      .create({
+        .create({
         firstName: req.body.firstName,
         lastName: req.body.lastName,
         email: req.body.email,
-        dateOfbirth: dateOfbirth.date,
-      })
-      .then((submitedUser) => res.send("Successfully registered"));
+        dateOfbirth: dateOfbirth.date
+    })
+    .then((submitedUser) => res.send("Successfuly registered"));
   } else {
     res.send(dateOfbirth.message);
   }
--- validateDate.js
+++ validateDate.js
@@ -6,14 +6,14 @@ function isValidDate(dateString) {
   if (!isValid) {
     return { valid: false, message: "Invalid date string" }; // you can get more fine grained information from moment
   } else {
-    if (moment().isBefore(dateValue)) {
+    if (moment(dateValue).isAfter(moment())) {
       return {
         valid: false,
         message: "Date of birth cannot be in the future",
       };
     }
+    return { valid: true, message: "Date is valid", date: dateValue.toDate() };
   }
-  return { valid: true, message: "Date is valid", date: dateValue.toDate() };
 }
 
 module.exports.isValidDate = isValidDate;
@@ -21,12 +21,14 @@ module.exports.isValidDate = isValidDate;
 // let validDateString = "22-12-1809";
 // let invalidDateString1 = "1809-12-22";
 // let invalidDateString2 = "12-22-1809";
+// let invalidDateString3 = "1809-22-12"
 // let futureDateString = "22-12-2021";
 
 // let sampleInputs = [
 //   validDateString,
 //   invalidDateString1,
 //   invalidDateString2,
+//   invalidDateString3
 //   futureDateString,
 // ];
 
